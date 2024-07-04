c6e7d59ddebb2ca6990ca8701fb544fede88c701
EasySouls <tarjanyicsanad@gmail.com>
2024-07-04 18:42:45 +0200

    Setup tests

--- src/civil-guards/civil-guards.service.spec.ts
+++ src/civil-guards/civil-guards.service.spec.ts
@@ -1,43 +1,43 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { mockDeep } from 'jest-mock-extended';
-import { PrismaService } from 'nestjs-prisma';
-import { CivilGuardsService } from './civil-guards.service';
-import { Prisma } from '@prisma/client';
+// import { Test, TestingModule } from '@nestjs/testing';
+// import { mockDeep } from 'jest-mock-extended';
+// import { PrismaService } from 'nestjs-prisma';
+// import { CivilGuardsService } from './civil-guards.service';
+// import { Prisma } from '@prisma/client';
 
-describe('CivilGuardsService', () => {
-  let service: CivilGuardsService;
-  const mockPrismaService = mockDeep<PrismaService>();
+// describe('CivilGuardsService', () => {
+//   let service: CivilGuardsService;
+//   const mockPrismaService = mockDeep<PrismaService>();
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      // TODO - Need to mock the PrismaService
-      providers: [
-        CivilGuardsService,
-        {
-          provide: PrismaService,
-          useValue: mockPrismaService,
-        },
-      ],
-      //controllers: [UsersController],
-    }).compile();
+//   beforeEach(async () => {
+//     const module: TestingModule = await Test.createTestingModule({
+//       // TODO - Need to mock the PrismaService
+//       providers: [
+//         CivilGuardsService,
+//         {
+//           provide: PrismaService,
+//           useValue: mockPrismaService,
+//         },
+//       ],
+//       //controllers: [UsersController],
+//     }).compile();
 
-    service = module.get<CivilGuardsService>(CivilGuardsService);
-  });
+//     service = module.get<CivilGuardsService>(CivilGuardsService);
+//   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+//   it('should be defined', () => {
+//     expect(service).toBeDefined();
+//   });
 
-  it('should find all users', async () => {
-    mockPrismaService.user.findMany.mockReturnValueOnce([]);
-    const result = await service.findAll();
-    expect(result).toEqual([]);
-  });
+//   it('should find all users', async () => {
+//     mockPrismaService.civilGuard.findMany.mockReturnValueOnce();
+//     const result = await service.findAll();
+//     expect(result).toEqual([]);
+//   });
 
-  it('should create a user', async () => {
-    const civilGuard: Prisma.CivilGuardCreateInput = {};
+//   it('should create a user', async () => {
+//     const civilGuard: Prisma.CivilGuardCreateInput = {};
 
-    const result = await service.create(civilGuard);
-    expect(result).toEqual(civilGuard);
-  });
-});
+//     const result = await service.create(civilGuard);
+//     expect(result).toEqual(civilGuard);
+//   });
+// });
--- src/departments/departments.controller.spec.ts
+++ src/departments/departments.controller.spec.ts
@@ -1,20 +1,54 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DepartmentsController } from './departments.controller';
 import { DepartmentsService } from './departments.service';
+import { PrismaService } from 'nestjs-prisma';
+import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
+
+const moduleMocker = new ModuleMocker(global);
 
 describe('DepartmentsController', () => {
   let controller: DepartmentsController;
+  let service: DepartmentsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [DepartmentsController],
-      providers: [DepartmentsService],
-    }).compile();
+      providers: [DepartmentsService, PrismaService],
+    })
+      .useMocker((token) => {
+        const results = ['test1', 'test2'];
+        if (token === DepartmentsService) {
+          return {
+            findAll: jest.fn().mockResolvedValue(results),
+          };
+        }
+        if (token === PrismaService) {
+          return {
+            department: {
+              findMany: jest.fn().mockResolvedValue(results),
+            },
+          };
+        }
+        if (typeof token === 'function') {
+          const mockMetadata = moduleMocker.getMetadata(token) as MockFunctionMetadata<any, any>;
+          const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+          return new Mock();
+        }
+      })
+      .compile();
 
-    controller = module.get<DepartmentsController>(DepartmentsController);
+    controller = await module.resolve<DepartmentsController>(DepartmentsController);
+    service = await module.resolve<DepartmentsService>(DepartmentsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return all departments', async () => {
+    const result = [];
+    jest.spyOn(service, 'findAll').mockImplementation(async () => result);
+
+    expect(await controller.findAll()).toBe(result);
   });
 });
